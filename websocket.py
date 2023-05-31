3864afdb53138c100e896dae2999d1750d6d0ae9
Aanshuu <e21cseu0085@bennett.edu.in>
2023-05-31 22:05:47 +0100

    Add websocket.py and index.html

---
+++ websocket.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+@socketio.on('connect')
+def handle_connect():
+    print('Client connected')
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    print('Client disconnected')
+
+@socketio.on('message')
+def handle_message(message):
+    print('Received message:', message)
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', port=8080)
+
